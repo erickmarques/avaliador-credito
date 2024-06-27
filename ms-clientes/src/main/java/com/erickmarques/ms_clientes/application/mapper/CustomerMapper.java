1b5c6835dbbd52450a0118127b3a511394075da3
Erick Marques <erick.andrade@mv.com.br>
2024-06-27 17:47:54 -0300

    colocando mapper para ignorar o id.

--- ms-clientes/src/main/java/com/erickmarques/ms_clientes/application/mapper/CustomerMapper.java
+++ ms-clientes/src/main/java/com/erickmarques/ms_clientes/application/mapper/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.erickmarques.ms_clientes.application.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.erickmarques.ms_clientes.application.representation.CustomerSaveRequest;
 import com.erickmarques.ms_clientes.application.representation.CustomerSaveResponse;
@@ -8,6 +9,9 @@ import com.erickmarques.ms_clientes.domain.Customer;
 
 @Mapper
 public interface CustomerMapper {
+    
+    @Mapping(target = "id", ignore = true) 
     Customer ToEntity(CustomerSaveRequest customerSaveRequest);
+    
     CustomerSaveResponse toDto(Customer customer);
 }
