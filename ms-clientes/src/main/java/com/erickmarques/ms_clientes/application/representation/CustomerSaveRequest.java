f0f484a5c73dc07b45c2a7b8e0ad6e481da41803
Erick Marques <erick.andrade@mv.com.br>
2024-06-27 22:32:06 -0300

    Inserindo validation de dto de inserir cliente.

--- ms-clientes/src/main/java/com/erickmarques/ms_clientes/application/representation/CustomerSaveRequest.java
+++ ms-clientes/src/main/java/com/erickmarques/ms_clientes/application/representation/CustomerSaveRequest.java
@@ -1,12 +1,23 @@
 package com.erickmarques.ms_clientes.application.representation;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CustomerSaveRequest {
+    
+    @NotBlank(message = "Favor informar o CPF!")
+    @CPF(message = "Favor informar um CPF válido!")
     private String cpf;
+
+    @NotBlank(message = "Favor informar o nome!")
     private String name;
+
+    @NotNull(message = "favor informar a idade!")
     private Integer age;
 }
--- ms-clientes/src/test/java/com/erickmarques/ms_clientes/MsClientesApplicationTests.java
+++ ms-clientes/src/test/java/com/erickmarques/ms_clientes/MsClientesApplicationTests.java
@@ -1,6 +1,5 @@
 package com.erickmarques.ms_clientes;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
