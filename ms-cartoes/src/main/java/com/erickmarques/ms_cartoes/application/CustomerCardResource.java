322bf13226433a039b14e9b44052ee9fd2b3a5e7
Erick Marques <erick.andrade@mv.com.br>
2024-06-30 19:54:00 -0300

    Criando controlador de cartões de cliente.

---
+++ ms-cartoes/src/main/java/com/erickmarques/ms_cartoes/application/CustomerCardResource.java
@@ -0,0 +1,27 @@
+package com.erickmarques.ms_cartoes.application;
+
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erickmarques.ms_cartoes.application.representation.CustomerCardResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/cliente-cartoes")
+@RequiredArgsConstructor
+public class CustomerCardResource {
+
+    private final CustomerCardService customerCardService;
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CustomerCardResponse>> pesquisarCartoesPorCpf(@RequestParam("cpf") String cpf){
+        List<CustomerCardResponse> customerCards = customerCardService.findCardsByCpf(cpf);
+        return ResponseEntity.ok(customerCards);
+    }
+}
