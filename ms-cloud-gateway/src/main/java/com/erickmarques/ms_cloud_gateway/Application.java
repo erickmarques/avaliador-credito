68c378408ee0ea8a6f390bc25c7125428a63b4ef
Erick Marques <erick.andrade@mv.com.br>
2024-07-21 19:01:04 -0300

    Iniciando projeto de gateway.

---
+++ ms-cloud-gateway/src/main/java/com/erickmarques/ms_cloud_gateway/Application.java
@@ -0,0 +1,27 @@
+package com.erickmarques.ms_cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+					.route( r -> r.path("/api/clientes/**").uri("lb://ms-clientes") )
+					.route( r -> r.path("/api/cartoes/**").uri("lb://ms-cartoes") )
+					.route( r -> r.path("/api/avaliacoes-credito/**").uri("lb://ms-avaliador-credito") )
+				.build();
+	}
+}
---
+++ ms-cloud-gateway/src/test/java/com/erickmarques/ms_cloud_gateway/ApplicationTests.java
@@ -0,0 +1,8 @@
+package com.erickmarques.ms_cloud_gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+}
